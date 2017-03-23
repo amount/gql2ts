14dd51decde21db27fc08ebbd33e3a11557f1679
Brett Jurgens <brett.jurgens@avant.com>
2017-03-23 15:20:14 -0500

    a bunch of lint fixes

--- fromQuery/index.ts
+++ fromQuery/index.ts
@@ -26,19 +26,19 @@ import {
   GraphQLNamedType,
 } from 'graphql';
 
-const doIt = (schema: GraphQLSchema | string, selection: string, typeMap: object = {}) => {
+const doIt: (schema: GraphQLSchema | string, query: string, typeMap: object) => any = (schema, query, typeMap = {}) => {
   const parsedSchema: GraphQLSchema = (schema instanceof GraphQLSchema) ? schema : buildSchema(schema);
-  const parsedSelection: DocumentNode = parse(selection);
+  const parsedSelection: DocumentNode = parse(query);
 
-  function isNonNullable(type: GraphQLType): type is GraphQLNonNull<any> {
+  function isNonNullable (type: GraphQLType): type is GraphQLNonNull<any> {
     return type instanceof GraphQLNonNull;
-  };
+  }
 
-  function isList(type: GraphQLType): type is GraphQLList<any> {
+  function isList (type: GraphQLType): type is GraphQLList<any> {
     return type instanceof GraphQLList;
-  };
+  }
 
-  const wrapList = (type: string): string => `Array<${type}>`;
+  const wrapList: (type: string) => string = type => `Array<${type}>`;
 
   const TypeMap: { [x: string]: string | undefined } = {
     ID: 'string',
@@ -49,62 +49,68 @@ const doIt = (schema: GraphQLSchema | string, selection: string, typeMap: object
     ...typeMap
   };
 
-  const printType = (type: string, isNonNull: boolean) => isNonNull ? type : `${type} | null`;
+  const printType: (type: string, isNonNull: boolean) => string = (type, isNonNull) => isNonNull ? type : `${type} | null`;
 
-  const handleInputObject = (type: GraphQLInputObjectType, isNonNull: boolean) => {
-    const variables: GraphQLInputField[] = Object.keys(type.getFields()).map(k => type.getFields()[k])
+  const handleInputObject: (type: GraphQLInputObjectType, isNonNull: boolean) => string = (type, isNonNull) => {
+    const variables: GraphQLInputField[] = Object.keys(type.getFields()).map(k => type.getFields()[k]);
     const builder: string = `{\n${variables.map(v => `    ${v.name}?: ${convertToType(v.type)};`).join('\n')}\n  }`;
     return printType(builder, isNonNull);
-  }
+  };
 
-  const handleEnum = (type: GraphQLEnumType, isNonNull) => {
+  const handleEnum: (type: GraphQLEnumType, isNonNull: boolean) => string = (type, isNonNull) => {
     const decl: string = type.getValues().map(en => `'${en.value}'`).join(' | ');
     return printType(decl, isNonNull);
-  }
+  };
 
-  const handleNamedTypeInput = (type: TypeNode, isNonNull: boolean) => {
+  const handleNamedTypeInput: (type: TypeNode, isNonNull: boolean) => string | undefined = (type, isNonNull) => {
     if (type.kind === 'NamedType' && type.name.kind === 'Name' && type.name.value) {
-      const newType: GraphQLType = parsedSchema.getType(type.name.value)
+      const newType: GraphQLType = parsedSchema.getType(type.name.value);
       if (newType instanceof GraphQLEnumType) {
         return handleEnum(newType, isNonNull);
       } else if (newType instanceof GraphQLInputObjectType) {
         return handleInputObject(newType, isNonNull);
       }
     }
-  }
+  };
 
-  const handleRegularType = (type: NamedTypeNode | GraphQLNamedType, isNonNull: boolean, replacement: string | null) => {
+  type RegularTypeSignature = (type: NamedTypeNode | GraphQLNamedType, isNonNull: boolean, replacement: string | null) => string;
+
+  const handleRegularType: RegularTypeSignature = (type, isNonNull, replacement) => {
     const typeValue: string = (typeof type.name === 'string') ? type.toString() : type.name.value;
     const showValue: string = replacement ? replacement : typeValue;
     const show: string = TypeMap[showValue] || (replacement ? showValue : 'any');
     return printType(show, isNonNull);
-  }
+  };
 
-  const convertVariable = (type: TypeNode, isNonNull: boolean= false, replacement: string | null= null): string => {
+  type VariableTypeSignature = (type: TypeNode, isNonNull?: boolean, replacement?: string | null) => string;
+
+  const convertVariable: VariableTypeSignature = (type, isNonNull= false, replacement= null) => {
     if (type.kind === 'ListType') {
-      return wrapList(convertVariable(type.type, false, replacement)) + printType('', isNonNull);
+      return wrapList(convertVariable(type.type, false, replacement)) + printType('', isNonNull!);
     } else if (type.kind === 'NonNullType') {
-      return convertVariable(type.type, true, replacement)
+      return convertVariable(type.type, true, replacement);
     } else {
-      return handleNamedTypeInput(type, isNonNull) || handleRegularType(type, isNonNull, replacement);
+      return handleNamedTypeInput(type, isNonNull!) || handleRegularType(type, isNonNull!, replacement!);
     }
-  }
+  };
 
-  const convertToType = (type: GraphQLOutputType | GraphQLInputType, isNonNull: boolean= false, replacement: string | null= null): string => {
+  type convertToTypeSignature = (type: GraphQLOutputType | GraphQLInputType, isNonNull?: boolean, replacement?: string | null) => string;
+
+  const convertToType: convertToTypeSignature = (type, isNonNull= false, replacement= null): string => {
     if (isList(type)) {
-      return wrapList(convertToType(type.ofType, false, replacement)) + printType('', isNonNull);
+      return wrapList(convertToType(type.ofType, false, replacement)) + printType('', isNonNull!);
     } else if (isNonNullable(type)) {
-      return convertToType(type.ofType, true, replacement)
+      return convertToType(type.ofType, true, replacement);
     } else if (type instanceof GraphQLEnumType) {
-      return handleEnum(type, isNonNull);
+      return handleEnum(type, isNonNull!);
     } else {
-      return handleRegularType(type, isNonNull, replacement);
+      return handleRegularType(type, isNonNull!, replacement!);
     }
-  }
+  };
 
   const UndefinedDirectives: Set<string> = new Set(['include', 'skip']);
 
-  const isUndefinedFromDirective = (directives: DirectiveNode[] | undefined): boolean => {
+  const isUndefinedFromDirective: (directives: DirectiveNode[] | undefined) => boolean = directives => {
     if (!directives || !directives.length) { return false; }
 
     const badDirectives: DirectiveNode[] = directives.filter(d => !UndefinedDirectives.has(d.name.value));
@@ -112,7 +118,7 @@ const doIt = (schema: GraphQLSchema | string, selection: string, typeMap: object
 
     if (badDirectives.length) {
       console.error('Found some unknown directives:');
-      badDirectives.forEach(d => console.error(d.name.value))
+      badDirectives.forEach(d => console.error(d.name.value));
     }
 
     if (hasDirectives) {
@@ -120,9 +126,9 @@ const doIt = (schema: GraphQLSchema | string, selection: string, typeMap: object
     } else {
       return false;
     }
-  }
+  };
 
-  interface ComplexTypeSignature {
+  interface IComplexTypeSignature {
     iface: string;
     isPartial: boolean;
     name: string;
@@ -131,18 +137,18 @@ const doIt = (schema: GraphQLSchema | string, selection: string, typeMap: object
     isFragment: boolean;
     isPartial: boolean;
     iface: string;
-    complexTypes: ComplexTypeSignature[];
+    complexTypes: IComplexTypeSignature[];
   }
 
-  const wrapPartial = (possiblePartial: IReturnType) => {
+  const wrapPartial: (possiblePartial: IReturnType) => string = possiblePartial => {
     if (possiblePartial.isPartial) {
       return `Partial<${possiblePartial.iface}>`;
     } else {
       return possiblePartial.iface;
     }
-  }
+  };
 
-  const getOperationFields = (operation: OperationTypeNode) => {
+  const getOperationFields: (operation: OperationTypeNode) => GraphQLObjectType = operation => {
     switch (operation) {
       case 'query':
         return parsedSchema.getQueryType();
@@ -153,15 +159,19 @@ const doIt = (schema: GraphQLSchema | string, selection: string, typeMap: object
       default:
         throw new Error('Unsupported Operation');
     }
-  }
+  };
 
-  const getChildSelections = (operation: OperationTypeNode, selection: SelectionNode, indentation: string = '', parent?: GraphQLType, isUndefined: boolean= false): IReturnType => {
+  type ChildSelectionsType =
+    (operation: OperationTypeNode, selection: SelectionNode, indentation: string, parent?: GraphQLType, isUndefined?: boolean)
+      => IReturnType;
+
+  const getChildSelections: ChildSelectionsType = (operation, selection, indentation= '', parent?, isUndefined= false): IReturnType => {
     let str: string = '';
     let field: GraphQLField<any, any>;
     let isFragment: boolean = false;
     let isPartial: boolean = false;
     let generatedTypeCount: number = 0;
-    let complexTypes: ComplexTypeSignature[] = [];
+    let complexTypes: IComplexTypeSignature[] = [];
 
     if (selection.kind === 'Field') {
       if (parent && isCompositeType(parent)) {
@@ -187,22 +197,24 @@ const doIt = (schema: GraphQLSchema | string, selection: string, typeMap: object
       str += indentation + selectionName + ': ';
 
       if (!!selection.selectionSet) {
-        let parent: GraphQLCompositeType | undefined;
-        if (!field) { console.log(selection, parent); }
-        const fieldType = getNamedType(field.type);
+        let newParent: GraphQLCompositeType | undefined;
+        if (!field) { console.log(selection, newParent); }
+        const fieldType: GraphQLNamedType = getNamedType(field.type);
         if (isCompositeType(fieldType)) {
-          parent = fieldType;
+          newParent = fieldType;
         }
 
-        const selections: IReturnType[] = selection.selectionSet.selections.map(sel => getChildSelections(operation, sel, indentation + '  ',  parent));
+        const selections: IReturnType[] =
+          selection.selectionSet.selections.map(sel => getChildSelections(operation, sel, indentation + '  ',  newParent));
+
         const nonFragments: IReturnType[] = selections.filter(s => !s.isFragment);
         const fragments: IReturnType[] = selections.filter(s => s.isFragment);
         const andOps: string[] = [];
         complexTypes.push(...selections.map(sel => sel.complexTypes).reduce((acc, arr) => { acc.push(...arr); return acc; }, []));
 
         if (nonFragments.length) {
-          const nonPartialNonFragments = nonFragments.filter(nf => !nf.isPartial);
-          const partialNonFragments = nonFragments.filter(nf => nf.isPartial);
+          const nonPartialNonFragments: IReturnType[] = nonFragments.filter(nf => !nf.isPartial);
+          const partialNonFragments: IReturnType[] = nonFragments.filter(nf => nf.isPartial);
 
           if (nonPartialNonFragments.length) {
             let builder: string = '';
@@ -231,7 +243,7 @@ const doIt = (schema: GraphQLSchema | string, selection: string, typeMap: object
           andOps.push(...fragments.map(wrapPartial));
         }
 
-        const childType = andOps.join(' & ');
+        const childType: string = andOps.join(' & ');
 
         str += convertToType(field.type, false, childType) + ';';
       } else {
@@ -239,17 +251,19 @@ const doIt = (schema: GraphQLSchema | string, selection: string, typeMap: object
         str += convertToType(field.type) + ';';
       }
     } else if (selection.kind === 'FragmentSpread') {
-      str = `IFragment${selection.name.value}`
+      str = `IFragment${selection.name.value}`;
       isFragment = true;
-      isPartial = isUndefinedFromDirective(selection.directives)
+      isPartial = isUndefinedFromDirective(selection.directives);
     } else if (selection.kind === 'InlineFragment') {
       const anon: boolean = !selection.typeCondition;
       if (!anon) {
-        const typeName = selection.typeCondition!.name.value;
+        const typeName: string = selection.typeCondition!.name.value;
         parent = parsedSchema.getType(typeName);
       }
 
-      const selections: IReturnType[] = selection.selectionSet.selections.map(sel => getChildSelections(operation, sel, indentation, parent, !anon));
+      const selections: IReturnType[] =
+        selection.selectionSet.selections.map(sel => getChildSelections(operation, sel, indentation, parent, !anon));
+
       let joinSelections: string = selections.map(s => s.iface).join('\n');
       isPartial = isUndefinedFromDirective(selection.directives);
 
@@ -269,20 +283,20 @@ const doIt = (schema: GraphQLSchema | string, selection: string, typeMap: object
       isPartial,
       complexTypes,
     };
-  }
+  };
 
-  const getVariables = (variables: VariableDefinitionNode[]) => {
-    return variables.map(v => {
+  const getVariables: (variables: VariableDefinitionNode[]) => string[] = variables => (
+    variables.map(v => {
       const optional: boolean = v.type.kind !== 'NonNullType';
       return `${v.variable.name.value}${optional ? '?:' : ':'} ${convertVariable(v.type)};`;
     })
-  }
+  );
 
   return parsedSelection.definitions.map(def => {
     if (def.kind === 'OperationDefinition') {
-      const name = def.name ? def.name.value : 'Anonymous';
-      let variableInterface = '';
-      let iface = '';
+      const name: string = def.name ? def.name.value : 'Anonymous';
+      let variableInterface: string = '';
+      let iface: string = '';
       if (def.variableDefinitions && !!def.variableDefinitions.length) {
         const variables: string[] = getVariables(def.variableDefinitions);
         variableInterface = `export interface ${name}Input {
@@ -291,30 +305,30 @@ const doIt = (schema: GraphQLSchema | string, selection: string, typeMap: object
       }
       iface += `export interface ${name} {\n`;
       let ret: IReturnType[] = def.selectionSet.selections.map(sel => getChildSelections(def.operation, sel, '  '));
-      let str: string[] = ret.map(x => x.iface)
+      let str: string[] = ret.map(x => x.iface);
       iface += str.join('\n');
       iface += `\n}`;
 
       return {
         variables: variableInterface,
         interface: iface,
-      }
+      };
     } else if (def.kind === 'FragmentDefinition') {
       const onType: string = def.typeCondition.name.value;
       const foundType: GraphQLType = parsedSchema.getType(onType);
-      let ret: IReturnType[] = def.selectionSet.selections.map(sel => getChildSelections('query', sel, '  ', foundType))
+      let ret: IReturnType[] = def.selectionSet.selections.map(sel => getChildSelections('query', sel, '  ', foundType));
       let str: string[] = ret.map(x => x.iface);
-      let iface = `export interface IFragment${def.name.value} {
+      let iface: string = `export interface IFragment${def.name.value} {
 ${str.join('\n')}
 }`;
       return {
         interface: iface,
         variables: ''
-      }
+      };
     } else {
       console.error('unsupported definition');
     }
-  })
-}
+  });
+};
 
 export default doIt;
