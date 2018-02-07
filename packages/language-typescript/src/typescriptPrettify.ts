77136ae2732dd3a9701c9a04006c127ff3b6a64e
Brett Jurgens <brett.jurgens@avant.com>
2018-02-07 15:56:49 -0600

    rm prettifier and allow prettier to be (optionally) used

--- packages/language-typescript/src/typescriptPrettify.ts
+++ packages/language-typescript/src/typescriptPrettify.ts
@@ -4,51 +4,23 @@
  * Default Options from:
  *  https://github.com/vvakame/typescript-formatter/tree/a49f0949c9760530365c5b874cf0e979bd010a04#read-settings-from-files
  */
-
-import * as ts from 'typescript';
-
-const TYPESCRIPT_OPTIONS: any = {
-  baseIndentSize: 0,
-  indentSize: 2,
-  tabSize: 2,
-  indentStyle: 2,
-  newLineCharacter: '\n',
-  convertTabsToSpaces: true,
-  insertSpaceAfterCommaDelimiter: true,
-  insertSpaceAfterSemicolonInForStatements: true,
-  insertSpaceBeforeAndAfterBinaryOperators: true,
-  insertSpaceAfterConstructor: false,
-  insertSpaceAfterKeywordsInControlFlowStatements: true,
-  insertSpaceAfterFunctionKeywordForAnonymousFunctions: false,
-  insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis: false,
-  insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets: false,
-  insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces: true,
-  insertSpaceAfterOpeningAndBeforeClosingTemplateStringBraces: false,
-  insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces: false,
-  insertSpaceAfterTypeAssertion: false,
-  insertSpaceBeforeFunctionParenthesis: false,
-  placeOpenBraceOnNewLineForFunctions: false,
-  placeOpenBraceOnNewLineForControlBlocks: false
-};
-
-const RULE_PROVIDER: any = new (ts as any).formatting.RulesProvider();
-RULE_PROVIDER.ensureUpToDate(TYPESCRIPT_OPTIONS);
-
-const applyEdits: (text: string, edits: ts.TextChange[]) => string = (text, edits) => {
-  let result: string = text;
-  for (let i: number = edits.length - 1; i >= 0; i--) {
-    let change: ts.TextChange = edits[i];
-    result = result.slice(0, change.span.start) + change.newText + result.slice(change.span.start + change.span.length);
-  }
-  return result;
-};
-
+import { format as prettierFormat } from 'prettier';
 const format: (text: string) => string = text => {
-  const sourceFile: ts.SourceFile = ts.createSourceFile('temp.ts', text, ts.ScriptTarget.Latest, true);
-
-  const edits: ts.TextChange[] = (ts as any).formatting.formatDocument(sourceFile, RULE_PROVIDER, TYPESCRIPT_OPTIONS);
-
-  return applyEdits(text, edits);
+  try {
+    // tslint:disable no-require-imports
+    const { format: formatter }: { format: typeof prettierFormat } = require('prettier');
+    return formatter(text, {
+      bracketSpacing: true,
+      parser: 'typescript',
+      semi: true,
+      singleQuote: true,
+      tabWidth: 2,
+      useTabs: false,
+    });
+  } catch (e) {
+    console.warn('To enable pretty-printing, please install prettier');
+    return text;
+  }
 };
 
 export default format;
