e19033b8d112b6021f6a8f27c533829108263cc8
Brett Jurgens <brett.jurgens@avant.com>
2018-03-09 09:38:41 -0600

    enum formatter test coverage

--- packages/language-typescript/src/__tests__/index-test.ts
+++ packages/language-typescript/src/__tests__/index-test.ts
@@ -2,8 +2,11 @@ import {
   interfaceExtendListToString,
   DEFAULT_NAME_QUERY,
   DEFAULT_FORMAT_INPUT,
-  DEFAULT_TYPE_PRINTER
+  DEFAULT_TYPE_PRINTER,
+  DEFAULT_ENUM_FORMATTER,
+  DEFAULT_ENUM_TYPE_BUILDER
 } from '../';
+import { GraphQLEnumValue } from 'graphql';
 
 describe('language-typescript', () => {
   describe('interfaceExtendListToString', () => {
@@ -43,6 +46,27 @@ describe('language-typescript', () => {
     });
     it('null', () => {
       expect(DEFAULT_TYPE_PRINTER('test', true)).toMatchSnapshot();
+    });
+  });
+
+  describe('DEFAULT_ENUM_FORMATTER', () => {
+    it('formats properly', () => {
+      expect(DEFAULT_ENUM_FORMATTER([
+        { value: 'a' } as GraphQLEnumValue,
+        { value: 'b' } as GraphQLEnumValue,
+      ])).toMatchSnapshot();
+    });
+  });
+
+  describe('DEFAULT_ENUM_TYPE_BUILDER', () => {
+    it('formats properly', () => {
+      expect(DEFAULT_ENUM_TYPE_BUILDER(
+        'test',
+        DEFAULT_ENUM_FORMATTER([
+          { value: 'a' } as GraphQLEnumValue,
+          { value: 'b' } as GraphQLEnumValue,
+        ]))
+      ).toMatchSnapshot();
     });
   });
 });
