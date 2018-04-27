4d97872cc32bdc574f7d4e953a3123d18b3782bc
Artur Bier <izatop@gmail.com>
2018-04-27 17:12:16 +0300

    An object as a default value fix
    When GraphQL schema provides an object as a default value
    DEFAULT_DOCUMENTATION_GENERATOR throws error "cannor covert object
    to primitive value'.

--- packages/language-typescript/src/index.ts
+++ packages/language-typescript/src/index.ts
@@ -89,7 +89,7 @@ const fixDescriptionDocblock: (description?: string) => string | undefined = des
 
 export const DEFAULT_DOCUMENTATION_GENERATOR: GenerateDocumentation = ({ description, tags = [] }) => (description || tags.length) ? `
   /**
-   * ${filterAndJoinArray([fixDescriptionDocblock(description), ...tags.map(({ tag, value }) => `@${tag} ${value}`)], '\n* ')}
+   * ${filterAndJoinArray([fixDescriptionDocblock(description), ...tags.map(({ tag, value }) => `@${tag} ${JSON.stringify(value)}`)], '\n* ')}
    */` : '';
 
 export const DEFAULT_OPTIONS: IFromQueryOptions = {
