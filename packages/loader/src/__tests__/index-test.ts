3735407ce538b203d2d5247ac68df689a064cca0
Brandon Herman <brandon.herman@avant.com>
2018-08-16 11:30:34 -0500

    Added --runInBand to tests

--- packages/loader/src/__tests__/index-test.ts
+++ packages/loader/src/__tests__/index-test.ts
@@ -14,35 +14,24 @@ const SOURCE: string = fs
 const RETURN_VALUE: string = 'GQL2TS RETURN VALUE';
 const FAKE_SCHEMA: PossibleSchemaInput = 'fake schema';
 
-let fsSpy: jest.Mock<typeof fs.writeFile>;
-let gql2tsSpy: jest.Mock<typeof fromQuery.default>;
-
 describe('Loader', () => {
-  beforeEach(() => {
-    fsSpy = jest
+  it('fails if no schema is provided', async () => {
+    const stats: Promise<Stats> = compiler(`./graphql/query.graphql`, {});
+    await expect(stats).rejects.toThrow('Schema must be provided');
+  });
+
+  it('writes a declaration file', async () => {
+    const fsSpy: jest.Mock<typeof fs.writeFile> = jest
       .spyOn(fs, 'writeFile')
       .mockImplementation((_file, data, callback) => {
         expect(data.toString()).toEqual(buildDeclaration(RETURN_VALUE));
         callback(null);
       });
 
-    gql2tsSpy = jest
+    const gql2tsSpy: jest.Mock<typeof fromQuery.default> = jest
       .spyOn(fromQuery, 'default')
       .mockImplementation(() => [{ result: RETURN_VALUE }]);
-  });
 
-  afterEach(() => {
-    jest.restoreAllMocks();
-  });
-
-  it('fails if no schema is provided', async () => {
-    const stats: Promise<Stats> = compiler(`./graphql/query.graphql`, {});
-    await expect(stats).rejects.toThrow('Schema must be provided');
-    expect(fsSpy).not.toHaveBeenCalled();
-    expect(gql2tsSpy).not.toHaveBeenCalled();
-  });
-
-  it('writes a declaration file', async () => {
     await compiler(`./graphql/query.graphql`, {
       schema: FAKE_SCHEMA,
     });
@@ -55,9 +44,23 @@ describe('Loader', () => {
       undefined,
       undefined,
     );
+
+    (fsSpy as any).mockRestore();
+    (gql2tsSpy as any).mockRestore();
   });
 
   it('writes a declaration file using typeMap and options', async () => {
+    const fsSpy: jest.Mock<typeof fs.writeFile> = jest
+      .spyOn(fs, 'writeFile')
+      .mockImplementation((_file, data, callback) => {
+        expect(data.toString()).toEqual(buildDeclaration(RETURN_VALUE));
+        callback(null);
+      });
+
+    const gql2tsSpy: jest.Mock<typeof fromQuery.default> = jest
+      .spyOn(fromQuery, 'default')
+      .mockImplementation(() => [{ result: RETURN_VALUE }]);
+
     let typeMapValue: Partial<ITypeMap> = { Test: 'object', Test2: 'string' };
     let optionsValue: Partial<IProvidedOptions> = {
       wrapList: (type: string) => `wrapped(${type})`,
@@ -78,5 +81,8 @@ describe('Loader', () => {
       typeMapValue,
       optionsValue,
     );
+
+    (fsSpy as any).mockRestore();
+    (gql2tsSpy as any).mockRestore();
   });
 });
