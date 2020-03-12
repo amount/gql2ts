29ac97ab62dbab19a496bc32e3146b36b82dad62
George Jia <georgejia@Georges-MacBook-Pro.local>
2020-03-12 15:07:06 -0500

    wip

--- packages/from-query/src/generate.ts
+++ packages/from-query/src/generate.ts
@@ -3,29 +3,57 @@
 import { IOperation, Selection, TypeDefinition, IInterfaceNode, IFieldNode, IDirectiveMap } from './ir';
 import { IFromQueryOptions } from '@gql2ts/types';
 
+const getReferenceType: (type: TypeDefinition) => string = type => {
+  switch (type.kind) {
+    case 'TypenameDefinition':
+      return Array.isArray(type.type) ? typeUnion(type.type) : type.type;
+    case 'TypeDefinition':
+      return `SelectionOn${type.type}`;
+    case 'ListTypeDefinition':
+      return getReferenceType(type.of);
+    case 'EnumTypeDefinition':
+      return type.type;
+    default:
+      throw new Error('Unsupported TypeDefinition');
+  }
+};
+
+const hasDirectives: (directives: IDirectiveMap) => boolean = directives =>
+  !!directives.include || !!directives.skip
+
+const generateSelectionName = (selection: IInterfaceNode | IFieldNode): string => {
+  switch (selection.kind) {
+    case 'Field':
+      return selection.name;
+    case 'InterfaceNode':
+      return selection.name;
+  }
+}
+
+// Print functions
+const typeUnion: (types: string[]) => string = types => types.join(' | ');
+const quoteValue: (value: string) => string = value => `'${value}'`;
+
+const printStringLiteral: (stringLiteral: string) => string = stringLiteral => `'${stringLiteral}'`;
+const printEnum: (values: string[]) => string = values => typeUnion(values.map(quoteValue));
+const printArray = (OPTIONS: IFromQueryOptions) => OPTIONS.wrapList;
+const printInterface: (OPTIONS: IFromQueryOptions, node: IInterfaceNode) => string = (OPTIONS, selection) => typeUnion(
+  selection.fragments.map(frag => {
+    const name = (frag.directives.gql2ts && frag.directives.gql2ts.arguments.interfaceName) ?
+      frag.directives.gql2ts.arguments.interfaceName :
+      OPTIONS.generateFragmentName((frag.typeDefinition as any).type)
+    return name;
+  })
+);
+
+
+
 export default (OPTIONS: IFromQueryOptions): (ir: IOperation) => string => {
-  const printArray = OPTIONS.wrapList;
 
   const printNullable: (underlyingType: string) => string = type => OPTIONS.printType(type, false);
   const wrapNullable: (type: TypeDefinition) => (nullablePrinter: typeof printNullable) => typeof printNullable = ({ nullable }) => nullablePrinter => str => (
     nullable ? nullablePrinter(str) : str
   );
-
-  const typeUnion: (types: string[]) => string = types => types.join(' | ');
-
-  const printStringLiteral: (stringLiteral: string) => string = stringLiteral => `'${stringLiteral}'`;
-
-  const printInterface: (node: IInterfaceNode) => string = selection => typeUnion(
-    selection.fragments.map(frag => {
-      const name = (frag.directives.gql2ts && frag.directives.gql2ts.arguments.interfaceName) ?
-        frag.directives.gql2ts.arguments.interfaceName :
-        OPTIONS.generateFragmentName((frag.typeDefinition as any).type)
-      return name;
-    })
-  );
-
-  const quoteValue: (value: string) => string = value => `'${value}'`;
-  const printEnum: (values: string[]) => string = values => typeUnion(values.map(quoteValue));
 
   const printType: (type: TypeDefinition | string, node: Selection, nameOverride?: string) => string = (type, node, nameOverride) => {
     if (typeof type === 'string') {
@@ -43,31 +71,13 @@ export default (OPTIONS: IFromQueryOptions): (ir: IOperation) => string => {
       case 'ListTypeDefinition':
         return nullWrapper(printArray(printType(type.of, node, nameOverride)));
       case 'InterfaceTypeDefinition':
-        return nullWrapper(printInterface(node as IInterfaceNode));
+        return nullWrapper(printInterface(OPTIONS, node as IInterfaceNode));
       case 'EnumTypeDefinition':
         return nullWrapper(printEnum(type.values))
       default:
         throw new Error('Unsupported TypeDefinition');
     }
   }
-
-  const getReferenceType: (type: TypeDefinition) => string = type => {
-    switch (type.kind) {
-      case 'TypenameDefinition':
-        return Array.isArray(type.type) ? typeUnion(type.type) : type.type;
-      case 'TypeDefinition':
-        return `SelectionOn${type.type}`;
-      case 'ListTypeDefinition':
-        return getReferenceType(type.of);
-      case 'EnumTypeDefinition':
-        return type.type;
-      default:
-        throw new Error('Unsupported TypeDefinition');
-    }
-  };
-
-  const hasDirectives: (directives: IDirectiveMap) => boolean = directives =>
-    !!directives['include'] || !!directives['skip']
 
   interface IPrintField {
     name: string
@@ -121,16 +131,18 @@ export default (OPTIONS: IFromQueryOptions): (ir: IOperation) => string => {
     }
 
     private buildDeclaration (selection: Selection): string {
+      const basePrintFields = {
+        name: selection.name,
+        type: selection.typeDefinition,
+        node: selection
+      };
+
       switch (selection.kind) {
         case 'Field':
-          const foo = !!selection.directives['include'] || !!selection.directives['skip']
-          const fieldName = this.buildDeclarations(getReferenceType(selection.typeDefinition), selection.selections);
           return printField({
-            name: selection.name,
-            type: selection.typeDefinition,
-            node: selection,
-            nameOverride: fieldName,
-            optional: foo
+            ...basePrintFields,
+            nameOverride: this.buildDeclarations(getReferenceType(selection.typeDefinition), selection.selections),
+            optional: hasDirectives(selection.directives)
           });
         case 'InterfaceNode':
           selection.fragments.map(frag => {
@@ -157,35 +169,21 @@ export default (OPTIONS: IFromQueryOptions): (ir: IOperation) => string => {
           const fragmentsHaveDirectives = !!selection.fragments.some(frag => hasDirectives(frag.directives))
 
           return printField({
-            name: this.generateSelectionName(selection),
-            type: selection.typeDefinition,
-            node: selection,
+            ...basePrintFields,
+            name: generateSelectionName(selection),
             optional: selectionHasDirectives || fragmentsHaveDirectives
           });
         case 'TypenameNode':
           return printField({
-            name: selection.name,
-            type: selection.typeDefinition,
-            node: selection
+            ...basePrintFields
           });
         case 'LeafNode':
           return printField({
-            name: selection.name,
-            type: selection.typeDefinition,
-            node: selection,
+            ...basePrintFields,
             optional: hasDirectives(selection.directives)
           });
         default:
           throw new Error('Unsupported Selection');
-      }
-    }
-
-    private generateSelectionName (selection: IInterfaceNode | IFieldNode): string {
-      switch (selection.kind) {
-        case 'Field':
-          return selection.name;
-        case 'InterfaceNode':
-          return selection.name;
       }
     }
   }
